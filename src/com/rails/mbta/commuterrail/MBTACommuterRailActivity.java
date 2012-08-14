b43e940e5451e63d51518362a276ebf13474a474
Matt Pietal <matthew.pietal@gmail.com>
2012-08-14 13:59:35 -0400

    Fix for rare crash on dialog disappears before task completes

--- src/com/rails/mbta/commuterrail/MBTACommuterRailActivity.java
+++ src/com/rails/mbta/commuterrail/MBTACommuterRailActivity.java
@@ -132,7 +132,7 @@ public class MBTACommuterRailActivity extends Activity {
                     iter.remove();
                     continue;
                 }
-                if (trip.service.startDate.compareTo(now) > 0 || trip.service.endDate.compareTo(now) < 0) {
+                if (trip.service.startDate.compareTo(now) > 0) {
                     iter.remove();
                     continue;
                 }
@@ -146,7 +146,9 @@ public class MBTACommuterRailActivity extends Activity {
             extras.putInt(SELECTED_LINE, selectedLine);
             intent.putExtras(extras);
 
-            progressDialog.dismiss();
+            if (progressDialog.isShowing()) {
+                progressDialog.dismiss();
+            }
 
             activity.startActivity(intent);
         }
