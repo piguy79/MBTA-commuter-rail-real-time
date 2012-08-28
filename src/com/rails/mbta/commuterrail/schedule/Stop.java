330387dc9ae53822bf7ab5106ba91bdbf4acb63c
Matt Pietal <matthew.pietal@gmail.com>
2012-08-28 16:48:52 -0400

    Add option to show schedule for a different day.

--- src/com/rails/mbta/commuterrail/schedule/Stop.java
+++ src/com/rails/mbta/commuterrail/schedule/Stop.java
@@ -6,7 +6,4 @@ public class Stop implements Serializable {
     private static final long serialVersionUID = 1144626477767489530L;
     public String stopId;
     public String stopName;
-    public String stopLon;
-    public String stopLat;
-
 }
